fd246765b50268e4156cc3263674eae22f058e8c
Seamoon Pandey <hackedasocial@gmail.com>
2024-02-01 20:05:23 +0545

    add timeline event

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -1,76 +1,72 @@
 import React from 'react';
-import './events.css'
-import {Link} from'react-router-dom'
-import {BiCoffeeTogo} from 'react-icons/bi'
-import {BsClipboardData} from 'react-icons/bs'
-import {GiDeadWood} from 'react-icons/gi'
-import {AiFillSecurityScan} from 'react-icons/ai'
+import './events.css';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { BiCoffeeTogo, BsClipboardData, GiDeadWood, AiFillSecurityScan } from 'react-icons/all';
 
+const eventDetails = [
+    {
+        title: 'Code with Coffee',
+        icon: <BiCoffeeTogo className='icon' />,
+        date: 'February 2023', // Set your specific dates
+        description: 'Code with Coffee is a competitive coding challenge hosted on Hacker Rank',
+    },
+    {
+        title: 'Datathon',
+        icon: <BsClipboardData className='icon' />,
+        date: 'March 2023',
+        description: 'Learn. Clean given dataset, generate insights, analyze, visualize and win',
+    },
+    {
+        title: 'Design Incubation',
+        icon: <GiDeadWood className='icon' />,
+        date: 'April 2023',
+        description: 'Tackle the growing challenges in the real world, with your next app design',
+    },
+    {
+        title: 'Babypwn: CTF',
+        icon: <AiFillSecurityScan className='icon' />,
+        date: 'May 2023',
+        description: 'Your first baby steps into the world of cybersecurity and ethical hacking',
+    },
+];
 
-
-
-
-
-
-
-
-const Events = () =>{
-    return(
+const Events = () => {
+    return (
         <div className='events'>
             <div className='blank'></div>
             <div className='title'>
                 <p>i-CES</p>
                 <h1>Learn and Compete your Interest</h1>
-                <h3>In celebration of the 35th anniversary of Paschimanchal Campus, Department of Electronics and Computer Engineering in association with i-CES is organizing Tech Parva, a celebration of Technology, Competition and Excellence.</h3>
+                <h3>
+                    In celebration of the 35th anniversary of Paschimanchal Campus, Department of Electronics and Computer
+                    Engineering in association with i-CES is organizing Tech Parva, a celebration of Technology, Competition, and
+                    Excellence.
+                </h3>
             </div>
-            <div className='content' id="events">
-                <a href="/codewithcoffee">
-                    <div className='scard'>
-                        <div className='image'>
-                            <BiCoffeeTogo className='icon'/>
-                        </div>
-                        <h3>Code with Coffee</h3>
-                        <div className='line'></div>
-                        <p>Code with Coffee is a competitive coding challenge hosted on Hacker Rank</p>
-                    </div>
-                </a>
-                <a href="/datathon">
-                    <div className='scard'>
-                        <div className='image'>
-                            <BsClipboardData className='icon'/>
-                        </div>
-                        <h3>Datathon</h3>
-                        <div className='line'></div>
-                        <p>Learn. Clean given dataset, generate insights, analyze, visualize and win</p>
-                    </div>
-                </a>
-                <a href="/designthinking">
-                    <div className='scard'>
-                        <div className='image'>
-                            <GiDeadWood className='icon'/>
-                        </div>
-                        <h3>Design Incubation </h3>
-                        <div className='line'></div>
-                        <p>Tackle the growing challenges in real world, with your next app design</p>
-                    </div>
-                </a>
-                <a href="/breakingintosecurity">
-                <div className='scard'>
-                    <div className='image'>
-                        <AiFillSecurityScan className='icon'/>
-                    </div>
-                    <h3>Babypwn: CTF </h3>
-                    <div className='line'></div>
-                    <p>Your first baby steps into the world of cybersecurity and ethical hacking</p>
-                </div>
-                </a>
-            </div>
-        </div>
-    )
-}
+            <VerticalTimeline >
+                {eventDetails.map((event, index) => (
+                    <VerticalTimelineElement
+                        key={index}
+                        className='vertical-timeline-element--work'
+                        date={event.date}
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={event.icon}
+                    >
+                        <a href={event.link} key={index}>
+                            <div className='scard'>
+                                <div className='image'>{event.icon}</div>
+                                <h3>{event.title}</h3>
+                                <div className='line'></div>
+                                <p>{event.description}</p>
+                            </div>
+                        </a>
 
+                    </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
+        </div>
+    );
+};
 
 export default Events;
-
-
-
