91815a105fa286ef4c20e5b56f47c8f4f02852b6
Seamoon Pandey <hackedasocial@gmail.com>
2024-02-01 23:28:04 +0545

    add new routes for Design Workshop and Security Session

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import CodewithCoffee from './pages/codewithcoffee/codewithcoffee'
 import Designthinking from './pages/designthinking/designthinking'
 import Security from './pages/security/security'
 import Datathon from './pages/datathon/datathon'
-
+import DesignWorkshop from './pages/designworkshop/designworkshop.jsx'
+import SecuritySession from './pages/security/security_session.jsx'
 
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route exact path='/' element={<Main />} />
-                    <Route exact path='/code-with-coffee' element={<CodewithCoffee />} />
-                    <Route exact path='/datathon-session' element={<CodewithCoffee />} />
-                    <Route exact path='/design-thinking' element={<Designthinking />} />
-                    <Route exact path='/breaking-into-security' element={<Security />} />
+                    <Route exact path='/codewithcoffee' element={<CodewithCoffee />} />
+                    <Route exact path='/design-workshop' element={<DesignWorkshop />} />
+                    <Route exact path='/designthinking' element={<Designthinking />} />
+                    <Route exact path='/breakingintosecurity' element={<Security />} />
+                    <Route exact path='/security-session' element={<SecuritySession />} />
                     <Route exact path='/datathon' element={<Datathon />} />
                 </Routes>
             </div>
---
+++ src/pages/security/security_session.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Copyright from '../../components/copyright/copyright';
+import RegisterPage from '../registerpage/registerpage';
+import './security.css';
+
+const Security = () => {
+    return (
+        <div className='security'>
+            <RegisterPage
+                title="BabyPwn: Capture the Flag"
+                time="10am onwards"
+                date="16th Mangsir, 2079"
+                location="CIT Hall, WRC"
+                countdown_time="12/02/2022"
+                urll="https://forms.gle/jWgSWp719UYM18f97"
+            />
+            <div className="detailsofevent">
+                <h3>About the event</h3>
+                <p>Cybersecurity is one of the most sought-after professions. Professionals that can secure contemporary infrastructure are in high demand. But going through security is a hassle in and of itself. </p>
+                <p>Our goal with BabyPwn is to assist those who are interested in security in starting their adventure. A discussion session with experts in the field discussing their experiences in security will precede a beginner-level Capture the Flag competition.</p>
+                <p>Finding concealed data, obtaining encryption keys, and getting around security measures are frequent problems. Teams of players often attack and defend computer systems during competitions, though the format might vary. Players may occasionally be given particular goals, like locating a lost password, or they may be left free to explore the system and locate anything they can. CTF competitions can be an entertaining and instructive approach to discover computer security and explore more.</p>
+                <h3>More Info:</h3>
+                <ul>
+                    <li>Talk session with industry insiders on how to get started in security</li>
+                    <li>A beginner level Capture the Flag Tournament</li>
+                    <li>Networking with Security Insiders</li>
+                </ul>
+                <h3>Point System</h3>
+                <p>To be Announced</p>
+            </div>
+            <Copyright />
+
+        </div>
+    )
+}
+
+export default Security;
